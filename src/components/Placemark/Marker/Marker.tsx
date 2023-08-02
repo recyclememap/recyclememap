99526285cd7f2caa7937a097c95f00df48686326
DianaGalter <diana.bobrova.spb@gmail.com>
2023-08-03 01:04:44 +0300

    feat: update placemarks
    (cherry picked from commit 8161f31619c6b1e58664a47b8a1a40e2e5c4b1c3)

--- src/components/Placemark/Marker/Marker.tsx
+++ src/components/Placemark/Marker/Marker.tsx
@@ -1,35 +1,26 @@
 import type { flatIconsKeys } from '@components/Icon/FlatIcons';
 import { flatIcons } from '@components/Icon/FlatIcons';
-import { Ring, MultiColouredRing } from '../Ring';
+import { Ring } from '../Ring';
 import { RingWrapper, Cutout } from './styles';
 
 const allIcons = Object.entries(flatIcons);
 
 export const Marker = ({ icons }: { icons: flatIconsKeys[] }) => {
   const iconsLength = icons.length;
-  if (iconsLength <= 4) {
-    return (
-      <RingWrapper>
-        {allIcons
-          .filter((item) => {
-            const [key] = item;
-            return icons.includes(key as flatIconsKeys);
-          })
-          .map((item, index, arr) => {
-            const [_, value] = item;
-            return (
-              <Ring
-                key={index}
-                index={index}
-                bgColor={value.color}
-                length={iconsLength}
-              />
-            );
-          })}
-        <Cutout />
-      </RingWrapper>
-    );
-  } else {
-    return <MultiColouredRing />;
-  }
+  const bgColors = allIcons
+    .filter((item) => {
+      const [key] = item;
+      return icons.includes(key as flatIconsKeys);
+    })
+    .map((item) => {
+      const [_, value] = item;
+      return value.color;
+    });
+
+  return (
+    <RingWrapper>
+      <Ring length={iconsLength} bgColors={bgColors} />
+      <Cutout />
+    </RingWrapper>
+  );
 };
