3d823a32e5574cdab90a70cc4ef788beb1d8e89c
DianaGalter <diana.bobrova.spb@gmail.com>
2023-08-03 01:31:22 +0300

    chore: remove theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
-import { CssBaseline } from '@mui/material';
-import { ThemeProvider as MUIThemeProvider } from '@mui/material/styles';
 import { lazy, Suspense } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { StoreContext, RootStore } from '@store/index';
 import { app } from './app.css';
-import { theme } from './theme';
 
 interface IApp {
   store: RootStore;
@@ -15,18 +12,15 @@ const HomePage = lazy(() => import('@views/HomePage/HomePage'));
 function App({ store }: IApp) {
   return (
     <StoreContext.Provider value={store}>
-      <MUIThemeProvider theme={theme}>
-        <CssBaseline />
-        <Suspense fallback={<div>Loading...</div>}>
-          <div className={app}>
-            <BrowserRouter>
-              <Routes>
-                <Route path="/" element={<HomePage />} />
-              </Routes>
-            </BrowserRouter>
-          </div>
-        </Suspense>
-      </MUIThemeProvider>
+      <Suspense fallback={<div>Loading...</div>}>
+        <div className={app}>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+            </Routes>
+          </BrowserRouter>
+        </div>
+      </Suspense>
     </StoreContext.Provider>
   );
 }
