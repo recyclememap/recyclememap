3815fa990752a4bc64aea657341b36f1147de4e9
Aleksei Lazev <51267520+NotAleksei@users.noreply.github.com>
2023-10-11 00:52:37 +0200

    Update src/components/dialogs/AddMarkerDialog/__tests__/addMarkerDialog.test.tsx
    Co-authored-by: Diana Bobrova <diana.bobrova.spb@gmail.com>

--- src/components/dialogs/AddMarkerDialog/__tests__/addMarkerDialog.test.tsx
+++ src/components/dialogs/AddMarkerDialog/__tests__/addMarkerDialog.test.tsx
@@ -62,7 +62,7 @@ describe('AddMarkerDialog logic', () => {
 
     expect(addButton).toBeDisabled();
 
-    await userEvent.click(screen.getByLabelText(DialogElements.WasteTypsLabel));
+    await userEvent.click(screen.getByLabelText(DialogElements.WasteTypesLabel));
     await userEvent.click(screen.getByText(DialogElements.WasteType));
 
     expect(addButton).toBeEnabled();
